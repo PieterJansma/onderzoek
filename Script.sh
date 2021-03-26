5d149ec6d5b8c61b3303fba592ec6eb87b8fdaca
PieterJansma <78420732+PieterJansma@users.noreply.github.com>
2021-03-26 17:27:07 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# Deze script telt hoe vaak een bepaald word voorkomt in een aangeven document/map.
 
 DIR=$1
 
 
 function h1 {
 	echo ${FUNCNAME[0]}
-	zless /net/corpora/twitter2/Tweets/2019/01/*.gz  | /net/corpora/twitter2/tools/tweet2tab text |\
-	grep -Eo '\w+'| grep -i -w "verdrietig" | wc -l
+	cat $FIlE |\ # Hier wordt de plek waar gezocht moet worden in geladen, als de database van rug gebruikt wordt gebruik je het colgende: 
+	#zless /net/corpora/twitter2/Tweets/2019/01/*.gz  | /net/corpora/twitter2/tools/tweet2tab text, in plaat van cat $FILE
+	grep -Eo '\w+'|\ # Hier wordt elk woord op een aparte regel gezet(tokenisen)
+	grep -i -w "word" |\ # Hier moet het woord dat naar gezocht moet worden in gezet, het programma zoekt naar de regels waar dat word zich bevind.
+	wc -l # telt elke regel waar het aangegeven woord zich bevind en telt alles bij elkaar op.
 
 }
 
 
-h1
+h1 
