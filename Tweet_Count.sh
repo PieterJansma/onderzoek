885e48e593ed0a89855322845cec3ec028704067
PieterJansma <78420732+PieterJansma@users.noreply.github.com>
2021-03-26 17:31:30 +0100

    Update Tweet_Count.sh

--- Tweet_Count.sh
+++ Tweet_Count.sh
@@ -2,12 +2,15 @@
 
 DIR=$1
 
+# Deze script telt hoe veel tweets er in totaal zijn gemaakt
+
 
 
 function h1 {
 	echo ${FUNCNAME[0]}
-	zless /net/corpora/twitter2/Tweets/2021/01/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i user |\
-	wc -l
+	zless /net/corpora/twitter2/Tweets/2021/01/*.gz  | /net/corpora/twitter2/tools/tweet2tab -i user |\ # hier moet een document komen waar alle gebruikersnamen
+													    #per tweet in staan, deze command gebruikt je als de RUG databass bezit.
+	wc -l # telt alle regels, aangezien elk gebruikersnaam op een aparte regel staat werkt het.
 }
 
 h1
